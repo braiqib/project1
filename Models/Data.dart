eac9beeadb8b8c3841efd7802e4d57704159ee26
braiqib <76533490+braiqib@users.noreply.github.com>
2020-12-23 21:27:01 +0300

    Create Data.dart

---
+++ Models/Data.dart
@@ -0,0 +1,15 @@
+
+// add here all field from the json https://jsonplaceholder.typicode.com/photos
+// this called Model class
+
+class DataModelClass {
+
+ int id;
+ String title;
+ String url;
+ String thumbnailUrl;
+
+ // Constructor
+ DataModelClass(this.id, this.title, this.url, this.thumbnailUrl);
+
+}
